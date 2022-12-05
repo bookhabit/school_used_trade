c307fdbec9b430bf58b5c16fde23ff894654669e
bookhabit <qnrgoqlt@naver.com>
2022-12-05 11:34:43 +0900

    카카오로그인api요청 - 컴포넌트 밖 함수로 분리시키기

--- src/pages/Kakao.js
+++ src/pages/Kakao.js
@@ -5,9 +5,27 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 import { LoginState } from "../states/LoginState";
 import { useRecoilState } from 'recoil';
+import { useSetRecoilState } from "recoil";
+
+
+  // 로그인 +  백엔드 서버에 유저 정보 요청하기
+  const getUserInfo = async (AUTHORIZE_CODE)=>{
+    let response
+    try {
+      response = await axios.post("http://localhost:4000/api/auth/callback/kakao",
+        {code:AUTHORIZE_CODE})
+    } catch(e) {
+      console.log(e);
+    };
+    console.log("유저정보가져오기:", response);
+    // 로컬 스토리지에 token 저장
+    await localStorage.setItem('token',response.data.token)
+    // 로컬스토리지에 User데이터 저장
+    await localStorage.setItem('user',JSON.stringify(response.data.user))    
+  }
 
 const Kakao = () => {
-  const [isLoggedIn,setIsLoggedIn] = useRecoilState(LoginState)
+  const setIsLoggedIn = useSetRecoilState(LoginState)
   const navigate = useNavigate();
   // URL에서 인가 코드 가져오기
   let code = new URL(window.location.href);
@@ -16,31 +34,9 @@ const Kakao = () => {
   // 인가코드 백엔드에 넘기기
   const AUTHORIZE_CODE = KAKAO_CODE;
 
-  // 로그인 +  백엔드 서버에 유저 정보 요청하기
-  const getUserInfo = async ()=>{
-    let response
-    try {
-      response = await axios.post("http://localhost:4000/api/auth/callback/kakao",
-        {code:AUTHORIZE_CODE})
-    } catch(e) {
-      console.log(e);
-    };
-    
-    console.log("유저정보가져오기:", response);
-    // 로컬 스토리지에 token 저장
-    await localStorage.setItem('token',response.data.token)
-    // 로컬스토리지에 User데이터 저장
-    await localStorage.setItem('user',JSON.stringify(response.data.user))    
-   
-  }
   // 카카오 로그인 수행 후 로그인상태변경과 메인화면 이동
   useEffect(()=>{
-    getUserInfo();
-    if(localStorage.getItem('user')){
-      setIsLoggedIn(true)
-    }else{
-      setIsLoggedIn(false)
-    }
+    getUserInfo(AUTHORIZE_CODE).then(setIsLoggedIn(true))
     navigate('/')
   },[])
 
