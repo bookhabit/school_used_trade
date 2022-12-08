ebabf9ee30a4426cb194dc976fd0a0dfceec069c
bookhabit <qnrgoqlt@naver.com>
2022-12-08 16:43:40 +0900

    로고이미지 변경

--- src/component/header/HeaderLeft.js
+++ src/component/header/HeaderLeft.js
@@ -1,12 +1,13 @@
 import React from "react";
 import logoImg from "../../svg/header/logo.svg";
+import mainLogo from "../../svg/header/main_logo.png";
 import { Link } from "react-router-dom";
 
 const HeaderLeft = () => {
   return (
     <div className="headerLeft">
       <Link to="/">
-        <img src={logoImg} alt="로고이미지" />
+        <img src={mainLogo} alt="로고이미지" style={{width:'170px'}} />
       </Link>
     </div>
   );
